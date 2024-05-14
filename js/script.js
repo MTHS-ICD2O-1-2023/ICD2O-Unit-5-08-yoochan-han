cf21e1e7053a7998352fde8534375046e106619c
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-14 02:20:45 +0000

    #

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yoochan
+// Created on: May 2024
 // This file contains the JS functions for index.html
+function calculate(){
+let left = document.getElementById("get_number1").value
+const Number1 = document.getElementById("get_number2").value
+let count = 0
+let answer
+while(true) {
+  if(left-Number1 <= 0) {
+    break
+  }else{
+    left -= Number1
+    count++
+  }
+}
+document.getElementById("Answer").innerHTML = "The answer is " + left + ' and ' + count + "R"
+}
